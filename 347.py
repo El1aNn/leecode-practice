eeeea3c61edced64f0c63f962944766163371d59
Elian <xzh_elian@163.com>
2024-09-25 20:15:24 +0800

    暴力 等会学一下堆

---
+++ 347.py
@@ -0,0 +1,12 @@
+# 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = defaultdict(int)
+        for num in nums:
+            d[num] += 1
+        return sorted(d, key=lambda x: (-d[x], x))[:k]
